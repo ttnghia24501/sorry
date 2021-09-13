a2899097733185da9d956418f904225e2efcfcdd
Tran Nghia <trantuannghia24501@gmail.com>
2021-09-13 12:46:52 +0700

    color

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 for (var i = 1; i <= 1000; i++) {
-  document.write("lần thứ: " + i + "<h2>Anh xin lỗi Em bé ạaaa</h2><br>");
+  document.write(
+    "lần thứ: " + i + "<h2>Anh xin lỗi Em bé ạaaa<span>❤❤<span></h2><br>"
+  );
 }
